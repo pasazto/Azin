0dd4f3323a66561de010d9777ffa527218379efb
احسان <69845291+pasazto@users.noreply.github.com>
2025-08-25 12:21:47 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,9 @@ self.addEventListener('install', event => {
       return cache.addAll([
         'index.html',
         'manifest.webmanifest',
-        'icon.png'
+        'icon.png',
+        'web-app-manifest-192x192',
+        'web-app-manifest-512x512',
       ]);
     })
   );
