a9e9c2b393eab154b70fbeb46b100c86776eca5b
احسان <69845291+pasazto@users.noreply.github.com>
2025-08-25 11:51:39 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('downtime-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.webmanifest',
+        'icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
